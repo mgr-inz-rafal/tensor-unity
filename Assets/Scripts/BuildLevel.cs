81b5ff2a1f2fe75b2d604ae7a99f5c6e541c0183
mgr-inz-rafal <rchabowski@gmail.com>
2019-01-19 17:46:38 +0100

    First drop

---
+++ Assets/Scripts/BuildLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildLevel : MonoBehaviour
+{
+    public GameObject brick00;
+    public GameObject docent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 12; ++i)
+        {
+            Instantiate(brick00, new Vector3(i, 0, 0), Quaternion.identity);
+            Instantiate(brick00, new Vector3(i, 11, 0), Quaternion.identity);
+        }
+        for(int i = 1; i < 11; ++i)
+        {
+            Instantiate(brick00, new Vector3(0, i, 0), Quaternion.identity);
+            Instantiate(brick00, new Vector3(11, i, 0), Quaternion.identity);
+        }
+
+        Instantiate(docent, new Vector3(1, 1, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-2.0f, 0.0f);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2.0f, 0.0f);
+        }
+    }
+}
