f869ce3579047fffe802c2ed65a1f21f37ee92a4
mgr-inz-rafal <rchabowski@gmail.com>
2019-01-19 18:08:08 +0100

    "Blocky" movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,17 @@ using UnityEditor;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-2.0f, 0.0f);
-        }
-        if(Input.GetKey(KeyCode.RightArrow))
+            Vector3 position_change = new Vector3(-1, 0, 0);
+            this.gameObject.transform.position += position_change;
+        } else if(Input.GetKeyUp(KeyCode.RightArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(2.0f, 0.0f);
+            Vector3 position_change = new Vector3(1, 0, 0);
+            this.gameObject.transform.position += position_change;
         }
     }
 }
