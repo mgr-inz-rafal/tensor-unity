016b519c938a0c959f202e3e8501b113003f2e58
mgr-inz-rafal <rchabowski@gmail.com>
2019-01-24 20:26:13 +0100

    Amygdalas rotate correctly

---
+++ Assets/Scripts/Amygdala.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amygdala : MonoBehaviour
+{
+    void SetVerticalCollider() {
+        Debug.Log("SetVerticalCollider");
+        BoxCollider2D x = this.gameObject.GetComponent<BoxCollider2D>();
+        x.size = new Vector2(0.6f, 1.0f);
+    }
+
+    void SetHorizontalCollider() {
+        Debug.Log("SetHorizontalCollider");
+        BoxCollider2D x = this.gameObject.GetComponent<BoxCollider2D>();
+        x.size = new Vector2(1.0f, 0.6f);
+    }
+
+}
