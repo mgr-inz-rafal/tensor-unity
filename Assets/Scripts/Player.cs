0910192a3374d0c013b88857cd1f153777cf45f8
mgr-inz-rafal <rchabowski@gmail.com>
2019-01-29 19:50:51 +0100

    Player makes only one step at a time (+ formatting :( )

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,65 +8,80 @@ public class Player : MonoBehaviour
 {
     const int MOVE_COUNT = 8;
 
+    bool block_next_step = false;
     int player_movement_count = 0;
     Tuple<float, float> player_movement_modifier = new Tuple<float, float>(0.0f, 0.0f);
 
     public void StepRight()
     {
-        face_right();
-        DoStep();
+        if (false == block_next_step)
+        {
+            block_next_step = true;
+            face_right();
+            DoStep();
+        }
     }
 
     public void StepLeft()
     {
-        face_left();
-        DoStep();
+        if (false == block_next_step)
+        {
+            block_next_step = true;
+            face_left();
+            DoStep();
+        }
     }
 
-    void face_right() {
+    void face_right()
+    {
         SpriteRenderer r = GetComponent<SpriteRenderer>();
         r.flipX = false;
         Debug.Log(WorldState.current_angle);
-        switch(WorldState.current_angle) {
+        switch (WorldState.current_angle)
+        {
             case 0:
-                player_movement_modifier = new Tuple<float, float>(1.0f/MOVE_COUNT, 0.0f);
+                player_movement_modifier = new Tuple<float, float>(1.0f / MOVE_COUNT, 0.0f);
                 break;
             case 90:
-                player_movement_modifier = new Tuple<float, float>(0.0f, 1.0f/MOVE_COUNT);
+                player_movement_modifier = new Tuple<float, float>(0.0f, 1.0f / MOVE_COUNT);
                 break;
             case 180:
-                player_movement_modifier = new Tuple<float, float>(-(1.0f/MOVE_COUNT), 0.0f);
+                player_movement_modifier = new Tuple<float, float>(-(1.0f / MOVE_COUNT), 0.0f);
                 break;
             case 270:
-                player_movement_modifier = new Tuple<float, float>(0.0f, -(1.0f/MOVE_COUNT));
+                player_movement_modifier = new Tuple<float, float>(0.0f, -(1.0f / MOVE_COUNT));
                 break;
         }
     }
 
-    void face_left() {
+    void face_left()
+    {
         SpriteRenderer r = GetComponent<SpriteRenderer>();
         r.flipX = true;
         Debug.Log(WorldState.current_angle);
-        switch(WorldState.current_angle) {
+        switch (WorldState.current_angle)
+        {
             case 0:
                 r.flipX = true;
-                player_movement_modifier = new Tuple<float, float>(-(1.0f/MOVE_COUNT), 0.0f);
+                player_movement_modifier = new Tuple<float, float>(-(1.0f / MOVE_COUNT), 0.0f);
                 break;
             case 90:
-                player_movement_modifier = new Tuple<float, float>(0.0f, -(1.0f/MOVE_COUNT));
+                player_movement_modifier = new Tuple<float, float>(0.0f, -(1.0f / MOVE_COUNT));
                 break;
             case 180:
-                player_movement_modifier = new Tuple<float, float>(1.0f/MOVE_COUNT, 0.0f);
+                player_movement_modifier = new Tuple<float, float>(1.0f / MOVE_COUNT, 0.0f);
                 r.flipX = true;
                 break;
             case 270:
-                player_movement_modifier = new Tuple<float, float>(0.0f, 1.0f/MOVE_COUNT);
+                player_movement_modifier = new Tuple<float, float>(0.0f, 1.0f / MOVE_COUNT);
                 break;
         }
     }
 
-    public void DoStep() {
-        if(player_movement_count == 0) {
+    public void DoStep()
+    {
+        if (player_movement_count == 0)
+        {
             player_movement_count = MOVE_COUNT;
             start_animation();
         }
@@ -75,11 +90,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player_movement_count > 0) {
+        if (player_movement_count > 0)
+        {
             perform_step();
             player_movement_count--;
-            if(player_movement_count == 0) {
+            if (player_movement_count == 0)
+            {
                 stop_animation();
+                block_next_step = false;
             }
         }
 
@@ -100,20 +118,24 @@ public class Player : MonoBehaviour
         gameObject.transform.position += position_change;
     }
 
-    void RotateLeft() {
+    void RotateLeft()
+    {
         this.transform.Rotate(new Vector3(0, 0, 1), 90);
     }
 
-    void RotateRight() {
+    void RotateRight()
+    {
         this.transform.Rotate(new Vector3(0, 0, 1), -90);
     }
 
-    void start_animation() {
+    void start_animation()
+    {
         Animator anim = GetComponent<Animator>();
         anim.speed = 1.0f;
     }
 
-    void stop_animation() {
+    void stop_animation()
+    {
         Animator anim = GetComponent<Animator>();
         anim.speed = 0.0f;
     }
