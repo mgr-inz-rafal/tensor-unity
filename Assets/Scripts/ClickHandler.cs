e7f707e4b0e220bd56e0dc6c2bfe256369902664
mgr-inz-rafal <rchabowski@gmail.com>
2019-01-29 19:57:18 +0100

    Formating + lock_rotation refactor

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -8,43 +8,50 @@ public class ClickHandler : MonoBehaviour
 
     int target_angle;
     bool rotate_player_left = false;
-    bool lock_rotation = false;
     int rotation_unlock_frame_count = ROTATION_LOCK_COUNT;
 
-    public void OnClick_Rotate_Right() {
-        if(WorldState.rotation_direction != 0) {
+    public void OnClick_Rotate_Right()
+    {
+        if (WorldState.rotation_direction != 0)
+        {
             return;
         }
 
-        if(lock_rotation) {
+        if (WorldState.lock_rotation)
+        {
             return;
         }
         rotation_unlock_frame_count = ROTATION_LOCK_COUNT;
-        lock_rotation = true;
+        WorldState.lock_rotation = true;
 
         WorldState.rotation_direction = 1;
         target_angle = WorldState.current_angle + 90;
-        if(target_angle == 360) {
+        if (target_angle == 360)
+        {
             target_angle = 0;
         }
 
         rotate_player_left = true;
     }
 
-    public void OnClick_Rotate_Left() {
-        if(WorldState.rotation_direction != 0) {
+    public void OnClick_Rotate_Left()
+    {
+        if (WorldState.rotation_direction != 0)
+        {
             return;
         }
 
-        if(lock_rotation) {
+        if (WorldState.lock_rotation)
+        {
             return;
         }
         rotation_unlock_frame_count = ROTATION_LOCK_COUNT;
-        lock_rotation = true;
+        WorldState.lock_rotation = true;
 
         WorldState.rotation_direction = -1;
         target_angle = WorldState.current_angle - 90;
-        if(target_angle < 0) {
+        if (target_angle < 0)
+        {
             target_angle += 360;
         }
 
@@ -53,8 +60,8 @@ public class ClickHandler : MonoBehaviour
 
     public void OnClick_RightArrow()
     {
-        Player playerScript = BuildLevel.docent_instance.GetComponent<Player>();       
-        if(playerScript == null)
+        Player playerScript = BuildLevel.docent_instance.GetComponent<Player>();
+        if (playerScript == null)
         {
             Debug.Log("Unable to find Player");
         }
@@ -66,8 +73,8 @@ public class ClickHandler : MonoBehaviour
 
     public void OnClick_LeftArrow()
     {
-        Player playerScript = BuildLevel.docent_instance.GetComponent<Player>();       
-        if(playerScript == null)
+        Player playerScript = BuildLevel.docent_instance.GetComponent<Player>();
+        if (playerScript == null)
         {
             Debug.Log("Unable to find Player");
         }
@@ -77,59 +84,73 @@ public class ClickHandler : MonoBehaviour
         }
     }
 
-    void AdjustRotation() {
-        switch(WorldState.rotation_direction) {
+    void AdjustRotation()
+    {
+        switch (WorldState.rotation_direction)
+        {
             case 0:
                 break;
             case 1:
                 WorldState.current_angle += 5;
-                if(WorldState.current_angle == 360) {
+                if (WorldState.current_angle == 360)
+                {
                     WorldState.current_angle = 0;
                 }
                 do_camera_rotation();
-                if(WorldState.current_angle == target_angle) {
+                if (WorldState.current_angle == target_angle)
+                {
                     finish_rotation();
                 }
                 break;
             case -1:
                 WorldState.current_angle -= 5;
-                if(WorldState.current_angle == -5) {
-                    WorldState.current_angle = 360-5;
+                if (WorldState.current_angle == -5)
+                {
+                    WorldState.current_angle = 360 - 5;
                 }
                 do_camera_rotation();
-                if(WorldState.current_angle == target_angle) {
+                if (WorldState.current_angle == target_angle)
+                {
                     finish_rotation();
                 }
                 break;
         }
     }
 
-    bool DocentNotMoving() {
+    bool DocentNotMoving()
+    {
         Rigidbody2D rb = BuildLevel.docent_instance.GetComponent<Rigidbody2D>();
         return rb.velocity.sqrMagnitude <= 0.10f;
     }
 
-    bool AmygdalasNotMoving() {
-        foreach(GameObject o in BuildLevel.amygdalas_instances) {
+    bool AmygdalasNotMoving()
+    {
+        foreach (GameObject o in BuildLevel.amygdalas_instances)
+        {
             Rigidbody2D rb = o.GetComponent<Rigidbody2D>();
-            if(rb.velocity.sqrMagnitude >= 0.10f) {
+            if (rb.velocity.sqrMagnitude >= 0.10f)
+            {
                 return false;
             }
         }
         return true;
     }
 
-    void UnlockRotation() {
-        if(!lock_rotation) {
+    void UnlockRotation()
+    {
+        if (!WorldState.lock_rotation)
+        {
             return;
         }
         rotation_unlock_frame_count--;
-        if(rotation_unlock_frame_count > 0) {
+        if (rotation_unlock_frame_count > 0)
+        {
             return;
         }
 
-        if(DocentNotMoving() && AmygdalasNotMoving()) {
-            lock_rotation = false;
+        if (DocentNotMoving() && AmygdalasNotMoving())
+        {
+            WorldState.lock_rotation = false;
         }
     }
 
@@ -139,11 +160,13 @@ public class ClickHandler : MonoBehaviour
         UnlockRotation();
     }
 
-    void finish_rotation() {
+    void finish_rotation()
+    {
         WorldState.rotation_direction = 0;
 
         BoxCollider2D collider = BuildLevel.docent_instance.GetComponent<BoxCollider2D>();
-        switch(WorldState.current_angle) {
+        switch (WorldState.current_angle)
+        {
             case 90:
                 Physics2D.gravity = new Vector3(9.8f, 0.0f, 0.0f);
                 break;
@@ -162,17 +185,20 @@ public class ClickHandler : MonoBehaviour
         WorldState.rotation_direction = 0;
     }
 
-    void DealWithActorRotations() {
+    void DealWithActorRotations()
+    {
         string message = rotate_player_left ? "RotateLeft" : "RotateRight";
         Player playerScript = BuildLevel.docent_instance.GetComponent<Player>();
         playerScript.SendMessage(message);
-        foreach(GameObject o in BuildLevel.amygdalas_instances) {
+        foreach (GameObject o in BuildLevel.amygdalas_instances)
+        {
             Amygdala amygdalaScript = o.GetComponent<Amygdala>();
             amygdalaScript.SendMessage(message);
         }
     }
 
-    void do_camera_rotation() {
+    void do_camera_rotation()
+    {
         Camera.main.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, WorldState.current_angle));
     }
 }
--- Assets/Scripts/WorldState.cs
+++ Assets/Scripts/WorldState.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class WorldState : MonoBehaviour
 {
-    public static byte[,] levelmap = new byte[12,12];
+    public static byte[,] levelmap = new byte[12, 12];
     public static int rotation_direction = 0;
     public static int current_angle = 0;
+    public static bool lock_rotation = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
