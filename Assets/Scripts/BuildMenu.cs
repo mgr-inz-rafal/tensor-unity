5d4803de57fc70e415d4e930f99ef00d735e4245
mgr-inz-rafal <rchabowski@gmail.com>
2019-02-11 19:16:44 +0100

    Adding Splash Screen

---
+++ Assets/Scripts/BuildMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMenu : MonoBehaviour
+{
+    public GameObject splashScreen;
+
+    void Start()
+    {
+        Instantiate(splashScreen, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+
+    }
+}
