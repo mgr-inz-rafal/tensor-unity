f08ea053322122c0461b5bcdb70e126f128a89f7
mgr_inz_rafal <rchabowski@gmail.com>
2023-10-07 01:35:41 +0200

    Ptasior shows up only occasionally

--- Assets/Scripts/Ptasior.cs
+++ Assets/Scripts/Ptasior.cs
@@ -10,24 +10,28 @@ public class Ptasior : MonoBehaviour
     public AudioClip crow;
     static bool first_voice = false;
     static bool second_voice = false;
+    static bool flying = false;
+    const int LICZBA_PAPIESKA = 2137;
 
     public void Fly()
     {
-//        return;
+        if (!flying) {
+            return;
+        }
         var pos = BuildLevel.ptasiorInstance.transform.position;
         pos.x += 0.05f;
         pos.y += (float)yacc;
         BuildLevel.ptasiorInstance.transform.position = pos;
         if (!first_voice  && pos.x > -0.5f && pos.x < (-0.5f+1.5f)) {
             String x = String.Format("should crow {0} {1}", first_voice , pos.x);
-            if (BuildLevel.rng.Next(2) == 0) {
+            if (BuildLevel.rng.Next(4) >= 1) {
                 GetComponent<AudioSource>().PlayOneShot(crow, 1.0f);
             }
             first_voice  = true;
         }
         if (!second_voice  && pos.x > 6.5f && pos.x < (6.5f+1.5f)) {
             String x = String.Format("should crow {0} {1}", first_voice , pos.x);
-            if (BuildLevel.rng.Next(2) == 0) {
+            if (BuildLevel.rng.Next(4) >= 1) {
                 GetComponent<AudioSource>().PlayOneShot(crow, 1.0f);
             }
             second_voice  = true;
@@ -44,6 +48,13 @@ public class Ptasior : MonoBehaviour
             return;
         }
 
+        if(!flying) {
+            var r = BuildLevel.rng.Next(1, 2700); 
+            if (r == LICZBA_PAPIESKA) {
+                flying = true;
+            }
+        }
+
         Fly();
     }
 
@@ -53,7 +64,7 @@ public class Ptasior : MonoBehaviour
 
         //var pos = new Vector3(5, 5, 0);
 
-        var randomYacc = BuildLevel.rng.NextDouble() * 0.025;
+        var randomYacc = BuildLevel.rng.NextDouble() * 0.019;
         if (posy > 5.5) {
             Ptasior.yacc = -randomYacc;
         }
@@ -63,5 +74,6 @@ public class Ptasior : MonoBehaviour
         BuildLevel.ptasiorInstance.transform.position = pos;
         Ptasior.first_voice  = false;
         Ptasior.second_voice  = false;
+        Ptasior.flying = false;
     }
 }
